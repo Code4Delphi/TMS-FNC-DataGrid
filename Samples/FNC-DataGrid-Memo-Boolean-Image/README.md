fac66a79aca2a70d892bbe95f975eece461e1bf2
César Cardoso <contato@code4delphi.com.br>
2026-05-22 23:06:05 -0300

    bk/2026-05-22-2306

---
+++ Samples/FNC-DataGrid-Memo-Boolean-Image/README.md
@@ -0,0 +1,5 @@
+# FNC DataGrid - Memo Fields, Boolean Fields & Image Blobs
+
+### Official documentation / Documentação oficial
+
+[Official documentation](https://download.tmssoftware.com/doc/tmsfncuipack/components/ttmsfncdatagrid/#memo-fields-boolean-fields-image-blobs)
---
+++ Samples/FNC-DataGrid-Memo-Boolean-Image/Src/MemoBooleanImage.View.pas
@@ -0,0 +1,168 @@
+unit MemoBooleanImage.View;
+
+interface
+
+uses
+  Winapi.Windows,
+  Winapi.Messages,
+  System.SysUtils,
+  System.Variants,
+  System.Classes,
+  System.Rtti,
+  System.Character,
+  Vcl.StdCtrls,
+  Vcl.Graphics,
+  Vcl.Controls,
+  Vcl.Forms,
+  Vcl.Dialogs,
+  VCL.ExtCtrls,
+  Data.DB,
+  FireDAC.Stan.Intf,
+  FireDAC.Stan.Option,
+  FireDAC.Stan.Error,
+  FireDAC.UI.Intf,
+  FireDAC.Phys.Intf,
+  FireDAC.Stan.Def,
+  FireDAC.Stan.Pool,
+  FireDAC.Stan.Async,
+  FireDAC.Phys,
+  FireDAC.Phys.SQLite,
+  FireDAC.Phys.SQLiteDef,
+  FireDAC.Stan.ExprFuncs,
+  FireDAC.Phys.SQLiteWrapper.Stat,
+  FireDAC.VCLUI.Wait,
+  FireDAC.Stan.Param,
+  FireDAC.DatS,
+  FireDAC.DApt.Intf,
+  FireDAC.DApt,
+  FireDAC.Comp.DataSet,
+  FireDAC.Comp.Client,
+  VCL.TMSFNCTypes,
+  VCL.TMSFNCUtils,
+  VCL.TMSFNCGraphics,
+  VCL.TMSFNCGraphicsTypes,
+  VCL.TMSFNCDataGridCell,
+  VCL.TMSFNCDataGridData,
+  VCL.TMSFNCDataGridBase,
+  VCL.TMSFNCDataGridCore,
+  VCL.TMSFNCDataGridRenderer,
+  VCL.TMSFNCCustomControl,
+  VCL.TMSFNCDataGrid,
+  VCL.TMSFNCCustomComponent,
+  VCL.TMSFNCDataGridDatabaseAdapter;
+
+type
+  TMemoBooleanImageView = class(TForm)
+    pnTop: TPanel;
+    TMSFNCDataGrid1: TTMSFNCDataGrid;
+    FDConnection1: TFDConnection;
+    FDQuery1: TFDQuery;
+    DataSource1: TDataSource;
+    TMSFNCDataGridDatabaseAdapter1: TTMSFNCDataGridDatabaseAdapter;
+    GroupBox2: TGroupBox;
+    btnClose: TButton;
+    btnOpenQuery: TButton;
+    GroupBox1: TGroupBox;
+    btnDescriptionRemove: TButton;
+    btnDescriptionAdd: TButton;
+    GroupBox3: TGroupBox;
+    btnAddColumn: TButton;
+    GroupBox4: TGroupBox;
+    ckShowMemoFields: TCheckBox;
+    ckShowBooleanFields: TCheckBox;
+    FDQuery1Id: TFDAutoIncField;
+    FDQuery1Name: TWideMemoField;
+    FDQuery1Description: TWideMemoField;
+    FDQuery1Active: TBooleanField;
+    FDQuery1Image: TBlobField;
+    FDQuery1Limited: TWideMemoField;
+    procedure FormCreate(Sender: TObject);
+    procedure btnOpenQueryClick(Sender: TObject);
+    procedure btnCloseClick(Sender: TObject);
+    procedure btnDescriptionRemoveClick(Sender: TObject);
+    procedure btnDescriptionAddClick(Sender: TObject);
+    procedure btnAddColumnClick(Sender: TObject);
+    procedure ckShowMemoFieldsClick(Sender: TObject);
+  private
+    procedure Configs;
+
+  public
+
+  end;
+
+var
+  MemoBooleanImageView: TMemoBooleanImageView;
+
+implementation
+
+{$R *.dfm}
+
+procedure TMemoBooleanImageView.FormCreate(Sender: TObject);
+begin
+  FDConnection1.Params.Database := '..\Data\MemoBooleanImage.db';
+  FDQuery1.Open;
+  Self.Configs;
+end;
+
+procedure TMemoBooleanImageView.ckShowMemoFieldsClick(Sender: TObject);
+begin
+  Self.Configs;
+end;
+
+procedure TMemoBooleanImageView.Configs;
+begin
+  TMSFNCDataGridDatabaseAdapter1.Columns[TMSFNCDataGrid1.ColumnIndexByHeader('Image')].PictureField := True;
+  TMSFNCDataGridDatabaseAdapter1.ShowMemoFields := ckShowMemoFields.Checked;
+
+  TMSFNCDataGridDatabaseAdapter1.ShowBooleanFields := ckShowBooleanFields.Checked;
+
+  //COLUMN Limited
+  var LLimitedIndex := TMSFNCDataGrid1.ColumnIndexByHeader('Limited');
+  var LLimitedCoumn := TMSFNCDataGrid1.Columns[LLimitedIndex];
+
+  LLimitedCoumn.Binding.BooleanTrueValue := 'Yes';
+  LLimitedCoumn.Binding.BooleanFalseValue := 'No';
+
+  TMSFNCDataGridDatabaseAdapter1.Columns[LLimitedIndex].CheckBoxField := True;
+  TMSFNCDataGridDatabaseAdapter1.Update;
+end;
+
+procedure TMemoBooleanImageView.btnOpenQueryClick(Sender: TObject);
+begin
+  FDQuery1.Open;
+  Self.Configs;
+end;
+
+procedure TMemoBooleanImageView.btnCloseClick(Sender: TObject);
+begin
+  FDQuery1.Close;
+end;
+
+procedure TMemoBooleanImageView.btnDescriptionAddClick(Sender: TObject);
+begin
+  TMSFNCDataGridDatabaseAdapter1.Columns[2].Header := 'Description';
+  TMSFNCDataGridDatabaseAdapter1.Columns[2].HTMLTemplate := '<font color="gcBlue">The description is: </FONT> <B><#Description> </B>';
+  TMSFNCDataGrid1.AutoSizeColumns;
+end;
+
+procedure TMemoBooleanImageView.btnDescriptionRemoveClick(Sender: TObject);
+begin
+  TMSFNCDataGridDatabaseAdapter1.Columns[2].HTMLTemplate := '';
+end;
+
+procedure TMemoBooleanImageView.btnAddColumnClick(Sender: TObject);
+begin
+  if not FDQuery1.Active then
+    FDQuery1.Open;
+
+  if TMSFNCDataGrid1.ColumnIndexByHeader('Department / Limit') >= 0 then
+    Exit;
+
+  var LColumn := TMSFNCDataGridDatabaseAdapter1.Columns.Add;
+  LColumn.Header := 'Department / Limit';
+  LColumn.HTMLTemplate := '<B><#Department></B><BR><font color="gcGreen">Limit: <#LimitValue></font>';
+
+  TMSFNCDataGrid1.AutoSizeGrid;
+end;
+
+end.
